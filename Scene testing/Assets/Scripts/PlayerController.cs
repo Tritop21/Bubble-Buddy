a2de8942037ad022e17eb8a46492beedcba79f90
Tritop21 <139844087+Tritop21@users.noreply.github.com>
2023-07-19 15:00:34 +0700

    Movement

--- Scene testing/Assets/Scripts/PlayerController.cs
+++ Scene testing/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5f;
+    Vector2 moveInput;
+
+    public bool IsMoving { get; private set; }
+
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +27,16 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput.x * speed, rb.velocity.y);
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+
+        IsMoving = moveInput != Vector2.zero;
     }
 }
