b0843d2564114401553d0876066d87707200350a
Tritop21 <139844087+Tritop21@users.noreply.github.com>
2023-07-22 17:25:30 +0700

    Semi-Working Lights
    Im going clinically insane because my code aint working

---
+++ Scene testing/Assets/Scripts/WorldLight.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+
+namespace WorldTime
+{
+    public class WorldLight : MonoBehaviour
+    {
+        private Light2D _light;
+        private float number;
+
+        [SerializeField]
+        private WorldTime _worldTime;
+
+        void Start()
+        {
+            _light = GetComponent<Light2D>();
+            _worldTime.WorldTimeChanged += OnWorldTimeChanged;
+            number = 1;
+        }
+
+        private void OnWorldTimeChanged(object sender, TimeSpan newTime)
+        {
+            _light.intensity = (PercentOfDay(newTime));
+        }
+
+        private void OnDestroy()
+        {
+            _worldTime.WorldTimeChanged -= OnWorldTimeChanged;
+        }
+
+        private float PercentOfDay(TimeSpan timeSpan)
+        {
+            if (timeSpan.TotalMinutes < 721)
+            {
+                return (float)timeSpan.TotalMinutes % WorldTimeConstant.MinutesInDay / WorldTimeConstant.MinutesInDay;
+            }
+            else if (timeSpan.TotalMinutes > 720)
+            {
+                return number - ((float)timeSpan.TotalMinutes % WorldTimeConstant.MinutesInDay / WorldTimeConstant.MinutesInDay);
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+
+    
+}
+
